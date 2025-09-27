538c21aaaf9582b2f2de76c5e525f3cb37d19782
pvbgayashan <pvbgayashan@gmail.com>
2025-09-27 19:38:06 +0530

    Removed old controllers, services, repositories and entities

--- public-lens-service/src/main/java/org/alignwithme/survey_service/repository/OldSurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package org.alignwithme.survey_service.repository;
-
-import org.alignwithme.survey_service.entity.Survey;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OldSurveyRepository extends MongoRepository<Survey, String> {
-
-    boolean existsById(String id);
-}
--- public-lens-service/src/main/java/org/alignwithme/survey_service/repository/QuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package org.alignwithme.survey_service.repository;
-
-import org.alignwithme.survey_service.entity.Question;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuestionRepository extends MongoRepository<Question, String> {}
