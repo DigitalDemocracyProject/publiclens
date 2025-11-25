3c86132929453fa2d8250848a7eb497da9f4f278
chathu-awm <awm.chathurika@gmail.com>
2025-11-26 00:41:23 +0530

    Google login integration with AWS Cognito

--- public-lens-portal/src/app/app.component.ts
+++ public-lens-portal/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(public auth: AuthService) {}
+}
