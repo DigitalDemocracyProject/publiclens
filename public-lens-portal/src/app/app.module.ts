74cb10fcd10bdedb7d19aa5dea2af171ce4de92a
chathu-awm <165944777+chathu-awm@users.noreply.github.com>
2026-05-02 00:29:21 +0530

    Display user details after sign in (#20)
    * Display user email on UI when user signin
    
    * Display user details after sign in
    
    * Code re-format
    
    * PR review fixes
    
    ---------
    
    Co-authored-by: pvbgayashan <pvbgayashan@gmail.com>

--- public-lens-portal/src/app/app.module.ts
+++ public-lens-portal/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,26 +11,22 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { FeaturesModule } from './features/features.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
-import { AnalysisViewerComponent } from './analysis-viewer/analysis-viewer.component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
     FeaturesModule,
-    SharedModule,
-    AnalysisViewerComponent
+    SharedModule
   ],
   providers: [
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptorsFromDi(), withFetch())
+    provideHttpClient()
   ],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule {}
